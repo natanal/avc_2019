f94a3e8d6c659837fbc8f5531ef5c64c6e6072e1
Nathanael Blankenship <nate090898@hotmail.com>
2018-12-07 11:28:27 -0600

    More comments and figuring out includes

--- avc_navigation/src/odometry_pub_node.cpp
+++ avc_navigation/src/odometry_pub_node.cpp
@@ -2,7 +2,9 @@
 //perform sensor fusion, estimate current odometry values
 //output values (Odometry message)
 
-
+//ROS includes
+#include <ros/ros.h>
+#include <sensor_msgs/Range.h> //not range.h
 //IMU values - velocity, acceleration, heading
 
 
@@ -10,3 +12,6 @@
 
 
 //Hall Effect - wheel speed
+
+
+//Subscribe to sensor node for value collection
