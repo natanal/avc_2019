4a01b5d8ca293c6e7b7783ed05f06fd7b6e3aec7
Nathanael Blankenship <nate090898@hotmail.com>
2018-12-07 12:39:53 -0600

    More includes, based on other people using odometry/sensor fusion

--- avc_navigation/src/odometry_pub_node.cpp
+++ avc_navigation/src/odometry_pub_node.cpp
@@ -4,7 +4,16 @@
 
 //ROS includes
 #include <ros/ros.h>
-#include <sensor_msgs/Range.h> //not range.h
+#include <tf/tf.h>//for tf transforms receiving and giving data
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+
+// Included messages
+#include <sensor_msgs/Range.h> //some other sensor_msg
+#include <nav_msgs/Odometry.h>  //create Odom estimation file
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/PoseStamped.h> //may not need
 //IMU values - velocity, acceleration, heading
 
 
