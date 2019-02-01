496161d63c0d72375a94ea01b0cd7f6752fdb659
Nathanael Blankenship <nate090898@hotmail.com>
2019-01-31 23:10:13 -0600

    added sections including messages, loop rate, and gps callback function

--- avc_navigation/src/odometry_pub_node.cpp
+++ avc_navigation/src/odometry_pub_node.cpp
@@ -24,3 +24,31 @@
 
 
 //Subscribe to sensor node for value collection
+//GPS Callback function
+int GPS_pos = 0;
+GPS_latitude=msg-->GPS_latitude
+interim_pos=0;
+
+//functions for each sensor
+
+//messages
+geometry_msg::Twist pos header.frame_id
+geometry_msg.linear.x=0
+geometry_msg.linear.y=0
+geometry_msg.linear.z=0
+geometry_msg.angular.x=0
+geometry_msg.angular.y=0
+geometry_msg.angular.z=0
+
+//publishing odometry messages without latching
+ros::Publisher odometry_pub = node_private.advertise<geometry_msgs::Twist>("odom", 10, false)
+ros::Subscriber
+
+//refresh rate specification
+ros::Rate loop_rate(refresh_rate)
+
+//loop to update GPS_pos and auto reset interim with each reading
+while (ros:ok())
+{
+
+}
